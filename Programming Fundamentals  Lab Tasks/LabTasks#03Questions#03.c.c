5d4e1250627b2d299aaa9651d4b320bac4db1170
Tasbiha-Naeem <tasbihanaeem123@gmail.com>
2025-01-14 22:01:10 +0500

    Add files via upload

---
+++ Programming Fundamentals  Lab Tasks/LabTasks#03Questions#03.c.c
@@ -0,0 +1,9 @@
+//Write a C program to print the following pattern using escape sequences. 
+#include<stdio.h>
+int main(){
+	printf("*\n");
+	printf("* *\n");
+	printf("* * *\n");
+	printf("* * * *\n");
+	return 0;
+}
