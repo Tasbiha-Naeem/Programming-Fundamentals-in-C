d60d153acdd12cbfdc1908579b638a695714408a
Tasbiha-Naeem <tasbihanaeem123@gmail.com>
2025-01-14 23:21:15 +0500

    Update LabTasks#03Question#05.c

--- Programming Fundamentals  Lab Tasks/LabTasks#03Question#05.c
+++ Programming Fundamentals  Lab Tasks/LabTasks#03Question#05.c
@@ -2,12 +2,12 @@
 decimal places exactly.*/
 #include<stdio.h>
 int main(){
-	float pie=3.14,radius,height,volume_of_cone;
+	float pi=3.14,radius,height,volume_of_cone;
 	printf("Enter the radius:\n");
 	scanf("%f",&radius);
 	printf("Enter the height:\n");
 	scanf("%f",&height);
-	volume_of_cone=0.3*(radius*radius*height);
+	volume_of_cone=1.0/3.0*pi*(radius*radius)*height;
 	printf("Volume of cone:%.3f",volume_of_cone);
 	return 0;
 }
