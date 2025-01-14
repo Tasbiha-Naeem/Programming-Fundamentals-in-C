8922beb365fc8b725f1b4de7ff0d9f88a46b355a
Tasbiha-Naeem <tasbihanaeem123@gmail.com>
2025-01-14 22:02:14 +0500

    Update LabTasks#03Questions#03.c.c

--- Programming Fundamentals  Lab Tasks/LabTasks#03Questions#03.c.c
+++ Programming Fundamentals  Lab Tasks/LabTasks#03Questions#03.c.c
@@ -1,9 +1,17 @@
-//Write a C program to print the following pattern using escape sequences. 
+/*Write a C program that takes two integer values as input from the user. Then swap the values taken 
+from the user and display the output of the variables. (Value of num1 should be stored in num2 and vice 
+versa)*/
 #include<stdio.h>
 int main(){
-	printf("*\n");
-	printf("* *\n");
-	printf("* * *\n");
-	printf("* * * *\n");
-	return 0;
+	int num1,num2,temp;
+	printf("Enter the value of num1:\n");
+	scanf("%d",&num1);
+	printf("Enter the value of num2:\n");
+	scanf("%d",&num2);
+	temp=num1;
+	num1=num2;
+	num2=temp;
+        printf("The value of num1 after swapping is %d\n",num1);
+        printf("The value of num2 after swapping is %d",num2);
+        return 0;
 }
