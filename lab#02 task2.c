b3da23595e3b1286a750d60d72330863dba1ecc2
Tasbiha Naeem <tasbihanaeem123@gmail.com>
2025-02-08 00:42:29 +0500

    Update lab#02 task2.c

--- lab#02 task2.c
+++ lab#02 task2.c
@@ -1,1 +1,19 @@
-
+#include<stdio.h>
+int main(){
+	int first_number;
+	int second_number;
+	printf("Enter the first number:\n");
+	scanf("%d",&first_number);
+	printf("Enter the second_number:\n");
+	scanf("%d",&second_number);
+	if(first_number>second_number){
+		printf("First number=%d is greater than second number=%d.\n",first_number,second_number);
+	}
+	else if(first_number==second_number){
+		printf("First number=%d is equal to second number=%d.\n",first_number,second_number);
+	}
+	else{
+		printf("First number=%d is smaller than the second number=%d.\n",first_number,second_number);
+	}
+	return 0;
+}
