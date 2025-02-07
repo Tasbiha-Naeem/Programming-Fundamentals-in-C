2c3b0d307766e94abfc43f70c2782d08d266ac53
Tasbiha Naeem <tasbihanaeem123@gmail.com>
2025-02-08 01:05:09 +0500

    Update lab#2 task3.c

--- lab#2 task3.c
+++ lab#2 task3.c
@@ -1,13 +1,7 @@
-#include<stdio.h>
-int main(){
-	int number;
-	printf("Enter the number:\n");
-	scanf("%d",&number);
-	if(number%2==0){
-		printf("The number you have entered is even.\n");
-	}
-	else{
-		printf("The number you have entered is odd.\n");
-	}
-	return 0;
+#include <stdio.h>
+
+int main() {
+    int num = 10;
+    (num%2==0)?printf("Even\n"):printf("Odd\n");
+    return 0;
 }
