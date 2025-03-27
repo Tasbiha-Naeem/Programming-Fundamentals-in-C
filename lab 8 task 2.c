fb9fd8004358259cc4d3f8849cc2cd685f0b9ae7
Tasbiha Naeem <tasbihanaeem123@gmail.com>
2025-03-27 19:28:04 +0500

    Create lab 8 task 2.c

---
+++ lab 8 task 2.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+	int student[10];
+	int highest_number=0;
+	float total_sum=0;
+	float average_number;
+	int minimum_number=100;
+	printf("Enter the marks of student`s:\n");
+	for(int i=0;i<10;i++){
+		scanf("%d",&student[i]);
+	}
+	for(int i=0;i<10;i++){
+		printf("The test score of student(%d) is %d\n",i+1,student[i]);
+	}
+	for(int i=0;i<10;i++){
+		if(highest_number<student[i]){
+			highest_number=student[i];
+		}
+		if(minimum_number>student[i]){
+			minimum_number=student[i];
+		}
+	}
+	for(int i=0;i<10;i++){
+		total_sum+=student[i];
+	}
+	average_number=total_sum/10;
+	printf("The highest number is %d\n",highest_number);
+	printf("The average number is %.0f\n",average_number);
+	printf("The minimum number is %d",minimum_number);
+	return 0;
+}
